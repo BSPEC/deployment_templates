07bf01001f6738b70f538b64878b29055ba0d28f
Ryan.Julyan <ryan@julyan.biz>
2022-10-02 21:07:59 +0200

    Change package to platform!

--- docker_docker-basic/hooks/post_gen_project.py
+++ docker_docker-basic/hooks/post_gen_project.py
@@ -6,23 +6,23 @@ print("**********Post Project Hook**************")
 
 BASE_DIR = os.path.dirname(os.getcwd())
 
-copy_from_dist = False if "{{cookiecutter.copy_from_package_dist}}" == "NO" else True
+copy_from_dist = False if "{{cookiecutter.copy_from_platform_dist}}" == "NO" else True
 
 if copy_from_dist:
-    package_dist_dir = r"{{cookiecutter.package_dist_dir|string}}".strip()
+    platform_dist_dir = r"{{cookiecutter.platform_dist_dir|string}}".strip()
 
-    package_dist_dir_type = True
-    if "{{cookiecutter.package_dist_dir_type}}" == "Absolute":
-        package_dist_dir_type = False
+    platform_dist_dir_type = True
+    if "{{cookiecutter.platform_dist_dir_type}}" == "Absolute":
+        platform_dist_dir_type = False
 
-    if package_dist_dir_type:
-        package_dist_dir_path = os.path.join(BASE_DIR, package_dist_dir)
+    if platform_dist_dir_type:
+        platform_dist_dir_path = os.path.join(BASE_DIR, platform_dist_dir)
     else:
-        package_dist_dir_path = os.path.join("", package_dist_dir)
+        platform_dist_dir_path = os.path.join("", platform_dist_dir)
 
-    print(f"Copy package dist folder from: '{package_dist_dir_path}'")
+    print(f"Copy platform dist folder from: '{platform_dist_dir_path}'")
 
     deployment_dist_dir = "{{cookiecutter.project_slug}}/dist"
     deployment_dist_dir_path = os.path.join(BASE_DIR, deployment_dist_dir)
 
-    shutil.copytree(package_dist_dir_path, deployment_dist_dir_path, dirs_exist_ok=True)
+    shutil.copytree(platform_dist_dir_path, deployment_dist_dir_path, dirs_exist_ok=True)
