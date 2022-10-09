bd87e0ad47db11c27c746f1565d5afa794e35713
Ryan Julyan <ryan@julyan.biz>
2022-10-09 07:41:20 +0200

    desktop pyinstaller windows

---
+++ desktop_pyinstaller-windows/{{cookiecutter.project_slug}}/create_desktop_installer_windows.py
@@ -0,0 +1,55 @@
+
+import os, shutil
+import secrets
+
+import PyInstaller.__main__
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+# my spec file in "dev\config" dir
+workdir = os.getcwd()
+# fn_msi_spec = os.path.join(workdir, 'main_msi.spec')
+
+# define the "dev\dist" and "dev\build" dirs
+# os.chdir("..")
+name = "{{cookiecutter.project_name}}"
+key = secrets.token_urlsafe(256)
+devdir = os.getcwd()
+distdir = os.path.join(devdir, "./desktop_windows/dist",name)
+builddir = os.path.join(devdir, "./desktop_windows/build")
+
+# call pyinstaller directly
+PyInstaller.__main__.run([
+    '{{ cookiecutter.desktop_entrypoint }}',
+    '--distpath', distdir,
+    '--workpath', builddir,
+    {% if cookiecutter.desktop_onefile == 'YES' %}'--onefile',{% endif %}
+    {% if cookiecutter.desktop_noconsole == 'YES' %}'--noconsole',{% endif %}
+    {% if cookiecutter.desktop_windowed == 'YES' %}'--windowed',{% endif %}
+    {% if cookiecutter.desktop_noconfirm == 'YES' %}'--noconfirm',{% endif %}
+    {% if cookiecutter.desktop_clean == 'YES' %}'--clean',{% endif %}
+    {%- for data in cookiecutter.desktop_add_data_points['add_data'] %}
+    '--add-data', '{{data}}',
+    {% endfor %}
+    '--additional-hooks-dir', 'hooks',
+    {%- for hidden_import in cookiecutter.desktop_hidden_imports['hidden_import'] %}
+    '--hidden-import', '{{hidden_import}}',
+    {% endfor %}
+    '--icon', '{{ cookiecutter.desktop_icon_ico }}',
+    '--key', key
+    '--name', name,
+    ])
+
+{%- for file_or_folder in cookiecutter.desktop_copy_files_and_folders_to_local_foler['files_or_folders'] %}
+copy_to_dir = f'{distdir}/{{file_or_folder}}'
+shutil.copy2("{{file_or_folder}}", copy_to_dir, dirs_exist_ok=True)
+{% endfor %}
+
