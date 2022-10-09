71da1a4c539178ba79b9a702d52f4a556e84306c
Ryan.Julyan <ryan@julyan.biz>
2022-10-09 12:22:06 +0200

    Checks for the requested deployment types

--- desktop_pyinstaller-lunix/hooks/pre_gen_project.py
+++ desktop_pyinstaller-lunix/hooks/pre_gen_project.py
@@ -1,1 +1,13 @@
+import sys
+import platform
+
+
 print("**********Pre Project Hook**************")
+my_os = platform.system()
+valid_os_names = ["Linux"]
+
+if my_os not in valid_os_names:
+    print("ERROR: you must be on a Linux machine to build for Linux")
+
+    # exits with status 1 to indicate failure
+    sys.exit(1)
